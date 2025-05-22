5f6158305a8f5f5d6b9bc6122495292fd7e569f7
africasergei <yhcho4243@gmail.com>
2025-05-22 19:09:12 +0900

    Revert "SecurityUser getId, getUserType 메소드 추가"
    This reverts commit c7642c0da77a88bc06b7a136bb30a4abdc716d7e.

--- backend/src/main/java/com/jobPrize/entity/common/SecurityUser.java
+++ backend/src/main/java/com/jobPrize/entity/common/SecurityUser.java
@@ -49,20 +49,5 @@ public class SecurityUser implements UserDetails {
 	public String getPassword() {
 		return user.getPassword();
 	}
-	
-	public Long getId() {
-	    return user.getId();
-	}
-	
-	public UserType getUserType() {
-	    String role = getAuthorities().stream()
-	        .map(GrantedAuthority::getAuthority)
-	        .findFirst()
-	        .orElse("ROLE_USER");
-
-	    return UserType.valueOf(role.replace("ROLE_", ""));
-	}
-	
-
 
 }
